f745f14c1d1320537f97c7fb428264b4467abaa2
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-05-22 14:20:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let namePrompt = prompt("Ismingizni kiriting !")
+let IDprompt = prompt("IDni kiriting !")
+let passwordPR = prompt("Parolni kiriting !")
+let obj = {
+    name: namePrompt,
+    ID: IDprompt,
+    password: passwordPR
+}
+console.log(obj);
